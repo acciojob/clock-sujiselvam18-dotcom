057897bcb9cc3702c47aeb278d5f8f5aa9cee85f
Sujitha.S <sujiselvam18@gmail.com>
2026-04-22 22:19:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function updateTime() {
+    const timer = document.getElementById("timer");
+    const now = new Date();
+
+    timer.textContent = now.toString();
+}
+
+// update every second
+setInterval(updateTime, 1000);
+
+// call once immediately (so it doesn't wait 1 sec)
+updateTime();
